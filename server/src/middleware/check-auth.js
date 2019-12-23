cbdf2db1827aca7f4003de10773cc31cb352cac1
Martin Albert <albert@killkitsch.com>
2019-12-23 17:26:54 +0100

    Middlewares added

--- server/src/middleware/check-auth.js
+++ server/src/middleware/check-auth.js
@@ -1,14 +1,16 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 module.exports = (req, res, next) => {
 	try {
-		const token = req.headers.authorization.split(" ")[1];
+		console.log(req.headers.authorization);
+		const token = req.headers.authorization.split(' ')[1];
 		const decoded = jwt.verify(token, process.env.JWT_KEY);
 		req.userData = decoded;
 		next();
 
 	} catch (error) {
-		return res.status(404).json({
+		return res.status(401).json({
 			message: 'Auth failed'
 		});
 	}
